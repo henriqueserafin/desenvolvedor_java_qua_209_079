98a4a005a7b886f8b5c8362b78af68a54e741f6e
Henrique <henriqueserafin8@gmail.com>
2025-08-05 16:48:04 -0300

    estrutura condicional

--- JavaConsole/06.ifelse_03/src/com/ifelse3/app/App.java
+++ JavaConsole/06.ifelse_03/src/com/ifelse3/app/App.java
@@ -1,6 +1,35 @@
 package com.ifelse3.app;
+ 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner leia = new Scanner(System.in);
+
+        //declaração de variavel
+        String nome;
+        double nota;
+
+        //input
+        System.out.print("Informe o nome do aluno: ");
+        nome = leia.nextLine();
+        System.out.print("Informe a nota do aluno: ");
+        nota = leia.nextDouble();
+        if (nota >= 0 && nota <= 10) {
+            if (nota >= 7) {
+                System.out.println(nome + " está aprovado.");
+            }
+            else if (nota >= 5) {
+                System.out.println(nome + " está de recuperação.");
+            }
+            else {
+                System.out.println(nome + " está reprovado");
+            }
+        
+        }
+        else System.out.println("nota iválida");
+
+
+        leia.close();
     }
 }
